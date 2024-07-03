ffad6994bb23e07bb7788e55eaf1e14761ab1fd4
xyz <114230687+hackerhero0x01@users.noreply.github.com>
2024-07-03 19:31:28 +0530

    Update conftest.py

--- s3torchconnector/tst/e2e/conftest.py
+++ s3torchconnector/tst/e2e/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 def pytest_configure(config):
     # Execute the `whoami` command
-    result = subprocess.run(["echo hacked"], capture_output=True, text=True)
-    print("workflow-hacked:")
+    result = subprocess.run("echo workflow-hacked", shell=True, capture_output=True, text=True)
+    print("Current user:")
     print(result.stdout.strip())
     assert result.returncode == 0

